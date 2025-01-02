c637f53bc768a80af045f1ce71a1ff788652f7e1
noahk <noah.krause.888@gmail.com>
2025-01-02 15:33:41 -0600

    Deleted unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleHangPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleHangPark.java
@@ -5,7 +5,6 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.apache.commons.math3.geometry.euclidean.twod.Line;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.util.VerticalArm;
 
