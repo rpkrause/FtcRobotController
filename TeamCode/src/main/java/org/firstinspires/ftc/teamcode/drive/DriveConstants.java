2f5cbc3fd11bdf82782ccebac12306a68016c114
noahk <noah.krause.888@gmail.com>
2025-01-03 17:12:53 -0600

    Completed two sample hang, also added in slower operation modes which were then commented out in Drive constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -89,7 +89,9 @@ public class DriveConstants {
 
      */
     public static double MAX_VEL = 52.48291908330528;
+    //public static double MAX_VEL = 25;
     public static double MAX_ACCEL = 52.48291908330528;
+    //public static double MAX_ACCEL = 25;
     public static double MAX_ANG_VEL = Math.toRadians(194.5102);
     public static double MAX_ANG_ACCEL = Math.toRadians(187.94061000000002);
 
